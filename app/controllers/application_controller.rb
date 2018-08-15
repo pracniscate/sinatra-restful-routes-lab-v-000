b1bb74e1f26a07c9e596a8a9042887df3daac4f1
Lorena <lorena.giddens@outlook.com>
2018-08-15 23:35:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     @recipe.save
-    redirect to '/recipes/#{@recipe.id}'
+    redirect to '/recipes/#{recipe.id}'
   end
 
   # renders a form to edit a recipe
