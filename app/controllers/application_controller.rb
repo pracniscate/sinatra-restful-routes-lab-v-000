91cf2c551e094f8c9f44925c834529f21432ba7a
Lorena <lorena.giddens@outlook.com>
2018-08-15 23:33:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/:id/edit' do
-    @recipe = Recipe.find_by_id(params[:id])
+    # @recipe = Recipe.find_by_id(params[:id])
+    @recipe = Recipe.find_by(id: params[:id])
     erb :edit
   end
 
